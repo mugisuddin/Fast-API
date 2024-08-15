c19a60e841562af76c10814abe6db789671b984a
Mugisuddin Shaikh <shaikhmugis96@gmail.com>
2024-08-15 13:54:20 +0530

    add new comments

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/sum")
+@app.get("/multiply")
+def calculate_sum(a: int, b: int):
+    result = a * b
+    return {"multiply": result}
+
+
+@app.get("/")
+def home():
+    return {"Welcome to my fast API function"}
+
+
+@app.get("/Add")
 def calculate_sum(a: int, b: int):
     result = a + b
-    return {"sum": result}
+    return {"Sum": result}
 
 
-
-
+@app.get("/Abid")
+def calculate_sum():
+    return {"Abid is amazing guy"}
 
 
