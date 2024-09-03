9a006a9bc6d9227732b0fe620dfdeb7284a736ed
Mugisuddin Shaikh <shaikhmugis96@gmail.com>
2024-09-04 03:43:32 +0530

    Add new update

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class post(BaseModel):
+    title: str
+    content: str
+    published : bool = True 
+
+
 
 @app.get("/multiply")
 def calculate_sum(a: int, b: int):
@@ -24,3 +34,20 @@ def calculate_sum():
     return {"Abid is amazing guy"}
 
 
+
+@app.get("/posts")
+def get_posts():
+    return {"Data": "this is my post"}
+
+
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+
+
+
+@app.post("/myposts")
+def create_posts(new_post:post):
+    print(new_post.title)
+    return {"Data": "new post"}
